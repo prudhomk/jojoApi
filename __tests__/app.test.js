4b29f22cf49f34aa394b2da5daaa1c07c7078588
Kalan Prudhomme <cleaver9@gmail.com>
2021-09-20 14:38:55 -0700

    created character model and table data

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,10 +3,30 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('character routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  test('creates a character', async () => {
+    const character = {
+      name: 'Joeseph Joestar',
+      japaneseName:'ジョセフ・ジョースター',
+      image: 'image.jpg',
+      abilities: 'Hamon, Clacker Balls, Clairvoyence, Strength, Intelligence',
+      nationality: 'British',
+      catchphrase: 'You\'re next line is...',
+      family: 'Jonathan Joestar, Jotaro Joestar, Lisa Lisa',
+      chapter: 'Battle Tendency, Stardust Crusaders, Diamond Is Unbreakable',
+      living: false,
+      isHuman: true
+    };
+
+    const res = await request(app).post('/api/v1/characters');
+    expect(res.body).toEqual({ ...character, id: '1' });
+  });
+
+
 
   afterAll(() => {
     pool.end();
