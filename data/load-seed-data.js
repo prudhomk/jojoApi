34906c8272121fdc5bd98441ddd6d32e371dc83b
Kalan Prudhomme <cleaver9@gmail.com>
2021-09-22 14:55:56 -0700

    commented out second promise

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -16,15 +16,15 @@ async function run() {
       })
     );
 
-    await Promise.all(
-      stands.map(stand => {
-        return client.query(`
-        INSERT INTO stands (name, japanese_name, alternate_name, image, stand_user, chapter, abilities, battlecry)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-        `,
-        [stand.name, stand.japaneseName, stand.alternateName, stand.image, stand.standUser, stand.chapter, stand.abilities, stand.battlecry]);
-      })
-    );
+    // await Promise.all(
+    //   stands.map(stand => {
+    //     return client.query(`
+    //     INSERT INTO stands (name, japanese_name, alternate_name, image, stand_user, chapter, abilities, battlecry)
+    //     VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+    //     `,
+    //     [stand.name, stand.japaneseName, stand.alternateName, stand.image, stand.standUser, stand.chapter, stand.abilities, stand.battlecry]);
+    //   })
+    // );
 
     console.log('seed data loaded');
   }
