df579076adb9ca676df3e2e7208f24b189c6a9c5
Kalan Prudhomme <cleaver9@gmail.com>
2021-09-23 11:28:08 -0700

    added seed data

--- data/standData.js
+++ data/standData.js
@@ -1,11 +1,20 @@
 const stands = [
-
+  {
+    name: 'The World',
+    alternateName: 'none',
+    japaneseName: 'ザ・ワールド(世界)',
+    image: 'image.jpg',
+    standUser: '2',
+    chapter: 'Stardust Crusaders, Steel Ball Run',
+    abilities: 'Time Stop, Super Speed, Super Strength',
+    battlecry: 'MUDAMUDAMUDA'
+  },
   {
     name: 'Hermit Purple',
     alternateName: 'none',
     japaneseName: 'ハーミットパープル（隠者の紫)',
     image: '',
-    standUser: '1',
+    standUser: '10',
     chapter: 'Stardust Crusaders',
     abilities: 'Transmit Messages, Grappling, Creating Images',
     battlecry: 'none'
@@ -15,11 +24,43 @@ const stands = [
     alternateName: 'none',
     japaneseName: 'スタープラチナ（星の白金)',
     image: 'image.jpg',
-    standUser: '2',
+    standUser: '18',
     chapter: 'Stardust Crusaders, Diamond Is Unbreakable, Stone Ocean',
     abilities: 'Time Stop, Super Speed, Super Strength, Breath, Star-Finger',
     battlecry: 'ORAORAORAORAORAORA'
-  }
+  },
+  {
+    name: 'Magician\'s Red',
+    alternateName: 'none',
+    japaneseName: 'マジシャンズレッド（魔術師の赤)',
+    image: 'image.jpg',
+    standUser: '19',
+    chapter: 'Stardust Crusaders',
+    abilities: 'Pyrokinesis, Super Strength, Super Speed, Cross Fire Hurricane, Red Bind, Life Detector',
+    battlecry: 'none'
+  },
+  {
+    name: 'Heirophant Green',
+    alternateName: 'none',
+    japaneseName: 'ハイエロファントグリーン（法皇の緑)',
+    image: 'image.jpg',
+    standUser: '20',
+    chapter: 'Stardust Crusaders',
+    abilities: 'Coiled Body, Marionette Controll, Emerald Splash',
+    battlecry: 'none'
+  },
+  {
+    name: 'Silver Chariot',
+    alternateName: 'none',
+    japaneseName: 'シルバーチャリオッツ（銀の戦車)',
+    image: 'image.jpg',
+    standUser: '21',
+    chapter: 'Stardust Crusaders, Vento Aureo',
+    abilities: 'Speed, Power, Precision, Armor, Sword Shot',
+    battlecry: 'HORAHORAHORA'
+  },
+
+  
 ];
 
 export default stands;
